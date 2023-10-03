b62482eef53897c08ad1554cbb8d1f62ab7e2259
Aditya Mishra <aditya@scriptlanes.com>
2023-10-03 22:13:00 +0530

    added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,19 @@
-import React from 'react';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import NoPage from './pages/NoPage';
+import Todo from './pages/todo/Todo';
+import TodoList from './pages/todo-list/TodoList';
+import Navbar from "./shared/navbar/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Navbar/>
+    <Routes>
+          <Route path="/" element={<TodoList />} />
+          <Route path="/todo" element={<Todo />} />
+          <Route path="*" element={<NoPage />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/shared/navbar/Navbar.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <>
+    <Link to='/'>Todo List</Link>
+    <Link to='/todo'>Todo</Link>
+    </>
+  )
+}
+
+export default Navbar
